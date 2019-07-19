1e30ea5d8300297d4154eab8fd1aa1e2714229a5
Adiii717 <adilm717@gmail.com>
2019-07-19 16:11:10 +0500

    add run in b

---
+++ main/sub_folder_a/project_a/run.sh
@@ -0,0 +1,9 @@
+#!/bin/ash
+
+if [ $1 == "prod" ]; then
+
+    echo -e "\x1B[31m start project name ${PROJECT_PATH}/${SCRIPT_NAME} withh prod \x1B[0m"
+else
+
+    echo -e "\x1B[31m start project name ${PROJECT_PATH}/${SCRIPT_NAME} withh $1 \x1B[0m" 
+fi
