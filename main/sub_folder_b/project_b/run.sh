b337c89b88d1a08e37a09827ac2298802b03837f
Adiii717 <adilm717@gmail.com>
2019-07-19 16:11:38 +0500

    fix comma issue

--- main/sub_folder_b/project_b/run.sh
+++ main/sub_folder_b/project_b/run.sh
@@ -2,8 +2,8 @@
 
 if [ $1 == "prod" ]; then
 
-    echo -e "\x1B[31m start project name ${PROJECT_PATH}/${SCRIPT_NAME} withh prod \x1B[0m" ' >> /run_project.sh
+    echo -e "\x1B[31m start project name ${PROJECT_PATH}/${SCRIPT_NAME} withh prod \x1B[0m" 
 else
 
-    echo -e "\x1B[31m start project name ${PROJECT_PATH}/${SCRIPT_NAME} withh $1 \x1B[0m" ' >> /run_project.sh
+    echo -e "\x1B[31m start project name ${PROJECT_PATH}/${SCRIPT_NAME} withh $1 \x1B[0m" 
 fi
